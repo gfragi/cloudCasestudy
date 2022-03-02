93a45ac457678035147c875bf6da8373853d7c8a
gfragi <gfragi@hua.gr>
2022-03-02 12:59:09 +0200

    add requirements && resources.py

---
+++ resources_calc.py
@@ -0,0 +1,10 @@
+# %%============== Import libraries =========
+import numpy as np
+import pandas as pd
+import warnings
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.utils import stats
+from scipy import stats
+
